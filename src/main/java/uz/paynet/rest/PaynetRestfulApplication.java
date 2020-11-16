0f49b4101be8b5a835acd715fbf772a25b685a71
BambooQ PM <asadganiev@gmail.com>
2020-11-16 09:48:25 +0500

    Maven: Created empty Spring Boot Maven Project

---
+++ src/main/java/uz/paynet/rest/PaynetRestfulApplication.java
@@ -0,0 +1,13 @@
+package uz.paynet.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaynetRestfulApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PaynetRestfulApplication.class, args);
+    }
+}
---
+++ src/test/java/uz/paynet/rest/PaynetRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.paynet.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaynetRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
