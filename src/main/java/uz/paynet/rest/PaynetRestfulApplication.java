8441e3bdd57f09c1a3727afb7a251bbda78e045b
BambooQ PM <asadganiev@gmail.com>
2020-11-17 14:39:55 +0500

    WIP: Chore: Working on user signup

--- src/main/java/uz/paynet/rest/PaynetRestfulApplication.java
+++ src/main/java/uz/paynet/rest/PaynetRestfulApplication.java
@@ -2,8 +2,11 @@ package uz.paynet.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {ErrorMvcAutoConfiguration.class}
+)
 public class PaynetRestfulApplication {
 
     public static void main(String[] args) {
